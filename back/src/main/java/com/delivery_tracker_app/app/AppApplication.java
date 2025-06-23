40a7f6c9d7a09377f7f45178920af215b6a0f2ed
oubellaismail <i.oubella1739@uca.ac.ma>
2025-06-23 12:51:35 +0100

    first commit

---
+++ back/src/main/java/com/delivery_tracker_app/app/AppApplication.java
@@ -0,0 +1,13 @@
+package com.delivery_tracker_app.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
